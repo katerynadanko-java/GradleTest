e993545a2f2219bd2e5308b976b70b88e0e23c4c
katerynadanko21 <katerynadanko21@gmail.com>
2021-03-07 16:49:34 +0200

    gradle project

---
+++ src/main/java/entity/Animal.java
@@ -0,0 +1,10 @@
+package entity;
+
+import lombok.Value;
+
+@Value
+public class Animal {
+    private String name;
+    private int age;
+
+}
---
+++ src/main/java/entity/Main.java
@@ -0,0 +1,8 @@
+package entity;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal kate = new Animal("Kate", 12);
+        System.out.println(kate);
+    }
+}
